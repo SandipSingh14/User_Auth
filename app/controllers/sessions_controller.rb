0de557bfb38e8c5f6b1dcc6cdebfbcbf5d1df2de
SandipSingh14 <sandip.singh@stackexpress.com>
2014-06-19 12:22:00 +0530

    controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,13 +6,14 @@ class SessionsController < ApplicationController
   def login_attempt
     authorized_user = User.authenticate(params[:username_or_email],params[:login_password])
     if authorized_user
-      flash[:notice] = "Wow Welcome again, you logged in as #{authorized_user.username}"
-      redirect_to(:action => 'home')
-    else
-      flash[:notice] = "Invalid Username or Password"
-      flash[:color]= "invalid"
-      render "login"	
-    end
+	session[:user_id] = authorized_user.id
+	flash[:notice] = "Wow Welcome again, you logged in as #{authorized_user.username}"
+	redirect_to(:action => 'home')
+  else
+	flash[:notice] = "Invalid Username or Password"
+        flash[:color]= "invalid"
+        render "login"	  
+  
   end  
 
 
