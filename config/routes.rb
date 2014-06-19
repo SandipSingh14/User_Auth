45ccefc5aa21a15acd8dcd57350aa411d49b40ad
SandipSingh14 <sandip.singh@stackexpress.com>
2014-06-19 10:58:09 +0530

    route.rb modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  match ':controller(/:action(/:id))(.:format)'
-
+root :to => "sessions#login"
+#match ':controller(/:action(/:id))(.:format)'
+get "signup", :to => "users#new"
+get "login", :to => "sessions#login"
+get "logout", :to => "sessions#logout"
+get "home", :to => "sessions#home"
+get "profile", :to => "sessions#profile"
+get "setting", :to => "sessions#setting"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -55,6 +60,6 @@ Rails.application.routes.draw do
   #   namespace :admin do
   #     # Directs /admin/products/* to Admin::ProductsController
   #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  #      resources :products
+  #     end
 end
